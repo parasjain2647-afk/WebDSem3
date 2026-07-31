3eddea048c55d6d7383a1b47f8ae62d30dba047b
Paras Jain <parasjain2647@gmail.com>
2026-07-31 11:49:56 +0530

    Day2

--- day2/a.js
+++ day2/a.js
@@ -1,18 +1,18 @@
-variables
+//variables
 console.log("===varibles===");
 var fName = "Paras";
 var lName = "Jain";
 var text = fName+" "+lName;
 console.log(text);
 
-conditionals
+//conditionals
 var a=40;
 var b=30;
 
 if(a>10) console.log("A is greater than 10.");
 else console.log("A is lesser than 10.");
 
-Functions
+//Functions
 function abes(){
     console.log("Welcome students.")
     console.log("Welcome to Node.js");
@@ -20,21 +20,21 @@ function abes(){
 abes();
 abes();
 
-function parameter
+//function parameter
 function greet(fName,lName){
     console.log("Hello",fName,lName);
 }
 greet("Paras","Jain");
 greet("Paras","Jain");
 
-function return
+//function return
 function sum(a,b){
     return a+b;
 }
 var add=sum(10,20)
 console.log("Sum:",add);
 
-Default functions
+//Default functions
 function fullName(fName="ABES", lName="EC"){
     return fName+" "+lName;
 }
@@ -52,7 +52,7 @@ var total= total(20,40,60);
 console.log("Total Marks:",total);
 console.log("Percentage",percentage(total),"%");
 
-while 
+//while 
 var i=0;
 while(i<=10){
     console.log(i);
