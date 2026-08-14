fc5197edf9eaa4d54ec7e0ac5bed32287f03d0ae
Paras Jain <parasjain2647@gmail.com>
2026-08-14 11:53:11 +0530

    Employee

---
+++ day3/emp.js
@@ -0,0 +1,11 @@
+const employee={
+    name:"Paras",
+    id:25,
+    dept:"AIML",
+    display(){
+    console.log("Name:", this.name);
+    console.log("Id:", this.id);
+    console.log("Dept:", this.dept);
+}
+};
+employee.display();
