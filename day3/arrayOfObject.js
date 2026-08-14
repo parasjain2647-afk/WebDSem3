a6711a86d72daf3e25288e7e00da94272bc75864
Paras Jain <parasjain2647@gmail.com>
2026-08-14 11:48:41 +0530

    arrayOfObject

---
+++ day3/arrayOfObject.js
@@ -0,0 +1,8 @@
+const student=[
+    {name:"Paras", id:25, dept:"AIML"},
+    {name:"Manav", id:24, dept:"AIML"},
+    {name:"Budhhu", id:23, dept:"AIML"},
+    {name:"Chamunda", id:22, dept:"AIML"}
+];
+
+console.log(student);
