3521c2653e132be5c1445d8d4024b514cbbf19fb
Paras Jain <parasjain2647@gmail.com>
2026-08-14 11:34:17 +0530

    Task3

--- day3/task3.js
+++ day3/task3.js
@@ -2,9 +2,9 @@ function calculatePercentage(a,b,c){
     return (a+b+c)/3;
 }
 
-let m1=prompt("Enter your Marks in English");
-let m2=prompt("Enter your Marks in Maths");
-let m3=prompt("Enter your Marks in Science");
+let m1=parseInt(prompt("Enter your Marks in English"));
+let m2=parseInt(prompt("Enter your Marks in Maths"));
+let m3=parseInt(prompt("Enter your Marks in Science"));
 
 let per=calculatePercentage(m1,m2,m3);
-console.log("Your percentage:", per);
+document.write("Your percentage:", per);
