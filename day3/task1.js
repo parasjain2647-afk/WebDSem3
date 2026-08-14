7252e16b30fccacff143f5286ba6ac3a51aa9747
Paras Jain <parasjain2647@gmail.com>
2026-08-14 11:09:16 +0530

    Task2

---
+++ day3/task1.js
@@ -0,0 +1,6 @@
+const name=prompt("Enter your name:");
+const id=prompt("Enter your id:");
+let branch=prompt("Enter your branch:");
+let dept=prompt("Enter your department:");
+
+console.log("Name:",name," Id:", id, "Branch:", branch, "Department:",dept);
