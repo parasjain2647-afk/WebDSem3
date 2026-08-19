97509a45678adbd1782cad4dd2d5f5d7210c9a7a
Paras Jain <parasjain2647@gmail.com>
2026-08-19 14:23:30 +0530

    ArrayMethods

---
+++ day4/arrayMethods.js
@@ -0,0 +1,12 @@
+let num=[10,20,30,40,50];
+console.log(num);
+
+console.log(num[0]);
+num.push(60);
+console.log(num);
+num.pop();
+console.log(num);
+num.unshift(5);
+console.log(num);
+num.shift();
+console.log(num);
