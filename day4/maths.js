9858147a8e7f00754bc70be563d6d7c0550caa5b
Paras Jain <parasjain2647@gmail.com>
2026-08-19 14:24:29 +0530

    Math

---
+++ day4/maths.js
@@ -0,0 +1,10 @@
+console.log("PI: ", Math.PI);
+
+let num=[10,20,30,40,50];
+console.log("Max", Math.max(...num));
+console.log("Min", Math.min(...num));
+console.log("Round", Math.round(9.8));
+console.log("Floor", Math.floor(1.9));
+console.log("Ceil", Math.ceil(1.9));
+console.log("Power", Math.pow(2,3));
+console.log("SqRt", Math.sqrt(16));
