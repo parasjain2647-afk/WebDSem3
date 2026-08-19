5b85f4a1b8079d1519ab29845f31b85dd38a8a26
Paras Jain <parasjain2647@gmail.com>
2026-08-19 14:25:06 +0530

    String

---
+++ day4/string.js
@@ -0,0 +1,21 @@
+let name="Paras Jain";
+console.log("length: ", name.length);
+let str="Welcome JavaScript";
+console.log("To uppercase: ", str.toUpperCase());
+console.log("To lowercase: ", str.toLowerCase());
+console.log("At 3: ", str.charAt(3));
+console.log("Index of JAVA: ", str.indexOf("Java"));
+let msg="JavaScript is easy. JavaScript is powerful.";
+console.log("Last Index of JavaScript: ", msg.lastIndexOf("JavaScript"));
+console.log("Slice: ",str.slice(0,7));
+let data="HTML, CSS JavaScript";
+console.log("Split: ", data.split());
+let text="I Love Java";
+console.log("Replace", text.replace("Java", "JavaScript"));
+
+let fn="Paras";
+let ln="Jain";
+console.log("COncat: ", fn.concat(" ", ln));
+
+let course="JavaScript";
+console.log("Include Script: ", course.includes("Script"), "Include Python: ", course.includes("Python"));
